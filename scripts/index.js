806f8d2c748aca4c3df0025d74f4eb4f4c1c9b32
MapIIeX <podhorhyi_2002@ukr.net>
2020-12-21 15:14:46 +0200

    initial-commit

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+import { videoPlayerInit } from "./videoPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+import { radioPlayerInit } from "./radioPlayer.js";
+
+const playerBtns = document.querySelectorAll(".player-btn");
+const playerBlocks = document.querySelectorAll(".player-block");
+const temp = document.querySelector(".temp");
+
+const disablePlayer = () => {
+    temp.style.display = 'none';
+    playerBtns.forEach((item) => item.classList.remove("active"));
+    playerBlocks.forEach((item) => item.classList.remove("active"));
+};
+
+playerBtns.forEach((btn, i) => btn.addEventListener("click", () => {
+    disablePlayer();
+    btn.classList.add("active");
+    playerBlocks[i].classList.add("active");
+}));
+
+videoPlayerInit();
+musicPlayerInit();
+radioPlayerInit();
